224925b9aed0dfc648e8ec8dcd1c20bd866ddd84
Shozab Hasan <shozab@isqr.io>
2022-12-06 22:19:39 +0500

    Implementation of the process to connect the Cardano wallet to Moodle via "User key authentication" Plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,231 +1,147 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import "./App.css";
-import { WalletInfo, WALLET_IDS } from "./wallets/base";
-import {
-  enable,
-  getAvailableWallets,
-  getBalance,
-  getChangeAddress,
-  getNetwork,
-  getRewardAddresses,
-  getUnusedAddresses,
-  getUsedAddresses,
-} from "./walletsGateway";
+import { Address, BaseAddress } from '@emurgo/cardano-serialization-lib-asmjs';
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
+import { WalletInfo, WALLET_IDS } from './wallets/base';
+import { enable, getAvailableWallets, getBalance, getChangeAddress, getNetwork, getRewardAddresses, getUnusedAddresses, getUsedAddresses } from './walletsGateway';
+var md5 = require('md5');
 
-function WalletCard(props: {
-  wallet: WalletInfo;
-  selected: boolean;
-  onClick: () => void;
-}) {
+let Buffer = require('buffer/').Buffer
+
+function WalletCard(props: { wallet: WalletInfo, handleClick: Function }) {
   return (
-    <div
-      className={`p-6 rounded-md bg-gray-900 transition-all shadow-lg cursor-pointer rounded-md ${
-        props.selected ? "border-txpink border-2" : ""
-      }`}
-      onClick={() => props.onClick()}
-    >
-      <div className="flex">
-        <img src={props.wallet.icon} alt="wallet-logo" width={28} height={28} />
-
-        <h3 className="text-xl font-medium tracking-wide ml-2">
-          {props.wallet.name}
-        </h3>
-      </div>
-      <div className="flex flex-col mt-4">
-        <p className="text-gray-300 text-sm">{`id: ${props.wallet.id}`}</p>
-        <p className="text-gray-300 text-sm">{`api version: ${props.wallet.apiVersion}`}</p>
-      </div>
-    </div>
-  );
-}
-
-type WalletState = {
-  balance: string;
-  network: string;
-  unusedAddresses: string[];
-  usedAddresses: string[];
-  rewardAddresses: string[];
-  changeAddress: string;
-};
-
-function AddressTableRow(props: { address: string }) {
-  return (
-    <tr className="odd:bg-gray-900 even:bg-gray-850 hover:bg-gray-750 transition-all">
-      <td className="text-sm px-6 py-3">{props.address}</td>
-    </tr>
-  );
-}
-
-function AddressesTable(props: { addresses: string[] }) {
-  return (
-    <table className="rounded-md overflow-hidden w-full">
-      <thead className="border-b border-gray-950">
-        <tr>
-          <th
-            className="bg-gray-900 py-3 px-6 text-sm text-left text-gray-400 font-normal"
-            scope="col"
-          >
-            Address
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.addresses.map((x) => (
-          <AddressTableRow key={x} address={x} />
-        ))}
-      </tbody>
-    </table>
+        <div className="p-8 bg-slate-800 hover:bg-slate-700 rounded-lg cursor-pointer flex h-full flex-col ">
+          <button className='flex flex-col' onClick={() => props.handleClick(props.wallet.id)}>
+            <div className="flex">
+                <img src={props.wallet.icon} alt='wallet-logo' width={28} height={28}/>
+                <span className='text-gray-400 ml-2 text-sm grow'>{`id: ${props.wallet.id}`}</span>
+            </div>
+            <h1 className="text-white font-bold line-clamp-1 mb-2 mt-4">{`Name: ${props.wallet.name}`}</h1>
+            <div className="flex mt-auto">
+                <p className="flex-1 text-gray-300 text-sm">{`api version: ${props.wallet.apiVersion}`}</p>
+            </div>
+            </button>
+        </div>
   );
 }
 
 function App() {
   const [wallets, setWallets] = useState([] as WalletInfo[]);
-  const [selectedWallet, setSelectedWallet] = useState<
-    WALLET_IDS | undefined
-  >();
   const [enabledWallet, setEnabledWallet] = useState<WalletInfo>();
-  const [walletState, setWalletState] = useState<Partial<WalletState>>();
+  const [balance, setBalance] = useState<string>();
+  const [network, setNetwork] = useState<string>();
+  const [address, setAddress] = useState<Address>();
   const [error, setError] = useState();
-
-  const mergeWalletState = useCallback((delta: Partial<WalletState>) => {
-    setWalletState((prev) => ({ ...prev, ...delta }));
-  }, []);
 
   useEffect(() => {
     setWallets(getAvailableWallets());
+  },[]);
+
+  const connectWallet = useCallback( async (walletId: WALLET_IDS) => {
+    try {
+      // clears the error state
+      setError(undefined);
+
+      // Enables the wallet
+      setEnabledWallet(await enable(walletId));
+
+      // Gets the enabled wallet balance
+      setBalance(await getBalance());
+
+      // Gets the enabled wallet network
+      setNetwork(await getNetwork());
+
+      setAddress(await getChangeAddress());
+
+    }catch(error: any) {
+      setError(error.message || 'unknown errorr');
+    }
   }, []);
 
-  useMemo(async () => {
-    try {
-      if (selectedWallet) {
-        // clears the error state
-        setError(undefined);
+  const getMoodleLink = async function (addr:any) {
 
-        // Enables the wallet
-        setEnabledWallet(await enable(selectedWallet!));
+    const raw = await getChangeAddress();
+    console.log(Buffer.from(raw, "hex"))
+    const changeAddress = Address.from_bytes(Buffer.from(raw, "hex")).to_bech32()
+    console.log(changeAddress)
 
-        // Gets the enabled wallet balance
-        mergeWalletState({ balance: await getBalance() });
+    var accaddr = md5(changeAddress)
 
-        // Gets the enabled wallet network
-        mergeWalletState({ network: await getNetwork() });
+    var MOODLEURL = 'http://x.x.x.x/' // @CONFIG You can set your Moodle URL here.
+    var MOODLEAPITOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' // @CONFIG You can set Moodle Webservice Token here.
+    
+    var url = MOODLEURL + '/webservice/rest/server.php?wstoken=' + MOODLEAPITOKEN + '&wsfunction=auth_userkey_request_login_url&moodlewsrestformat=json';
 
-        // Gets the list of unused addresses by the wallet
-        mergeWalletState({ usedAddresses: await getUsedAddresses() });
+    const postdata = new FormData();
+    postdata.append('user[firstname]', "anonymous");
+    postdata.append('user[lastname]', accaddr.toLowerCase());
+    postdata.append('user[email]', (accaddr+"@pocre.io").toLowerCase());
+    postdata.append('user[username]', accaddr.toLowerCase());
+    postdata.append('user[idnumber]', accaddr.toLowerCase());
 
-        // Gets the list of unused addresses by the wallet
-        mergeWalletState({ unusedAddresses: await getUnusedAddresses() });
-
-        mergeWalletState({ rewardAddresses: await getRewardAddresses() });
-
-        mergeWalletState({ changeAddress: await getChangeAddress() });
+    fetch(url, {
+      method: 'POST',
+      body: postdata
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      if(data['errorcode'])
+        alert(data['message'])
+      else {
+        alert(data['loginurl']) // This is the loginurl which can be used to access the Moodle without entring any further details.
       }
-    } catch (error: any) {
-      setError(error.message || "unknown error");
-    }
-  }, [mergeWalletState, selectedWallet]);
+    });
+  };
 
   return (
-    <div className="bg-gray-950 w-screen h-full">
-      <div className="container mx-auto px-4 sm:px-6 max-w-6xl p-16 text-white">
-        <header className="flex flex-col mb-4">
-          <div className="flex">
-            <img src="/logo.svg" className="mr-4 w-5 h-5" alt="TxPipe Logo" />
-            <p className="text-sm">Starter Kit provided by TxPipe</p>
-          </div>
+    <div className="w-screen h-screen bg-gray-900 overflow-auto">
+        <div className="container max-w-6xl p-16  h-full w-full">
+            <header className="mb-3 py-6 w-full flex flex-col justify-between">
+                <div className='flex'>
+                    <img src="/logo.svg" className="mr-4 h-6" alt="TxPipe Logo" />
+                    <h2 className="text-m text-gray-400 font-normal">Starter Kit provided by TxPipe</h2>
+                </div>
+                
+                <h3 className="text-3xl text-gray-200 font-extrabold mt-4">Wallet Connector</h3>
+                <div className="mt-8 rounded-lg border border-blue-500 bg-blue-600 bg-opacity-10 p-4 text-white mb-4">
+                    <h1 className="font-bold">Connect to a Wallet</h1>
+                    <h3 className="text-sm text-blue-500 mt-2">Select which wallet to connect and perform basic interactions.</h3>
+                </div>
+            </header>
 
-          <h1 className="text-4xl font-light mt-2">Wallet Connector</h1>
-          <p className="text-gray-400 mt-8">
-            Select which wallet to connect and perform basic interactions.
-          </p>
-        </header>
+            {/* Available wallets */}
+            {wallets.length ? <>
+              <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                { wallets.map((wallet: WalletInfo) => <WalletCard key={wallet.id} wallet={wallet} handleClick={connectWallet}/>)}
+              </div>
+            </> : <h3 className="text-3xl text-gray-200 font-extrabold mt-4">No Wallets were found</h3>}
 
-        {/* Available wallets */}
-        {wallets.length ? (
-          <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {wallets.map((wallet: WalletInfo) => (
-              <WalletCard
-                key={wallet.id}
-                wallet={wallet}
-                selected={selectedWallet === wallet.id}
-                onClick={() => {
-                  setSelectedWallet(wallet.id);
-                }}
-              />
-            ))}
-          </div>
-        ) : (
-          <p className="text-red-500">No wallets were found in your browser.</p>
-        )}
+            {/* Connected Wallet information */}
+            <div className='mt-8'>
+              { enabledWallet ? 
+              <>
+                <h3 className="text-l text-white font-extrabold mt-4">{`Connected to ${enabledWallet.name}`}</h3>
+                <h3 className="text-sm text-gray-200 mt-4">{balance ? `Wallet Balance: ${balance}` : null}</h3>
+                <h3 className="text-sm text-gray-200 mt-2">{network ? `Connected to: ${network}` : null}</h3>
+                  
+                { address ?
+                <><button className="mt-8 rounded-lg border border-blue-500 bg-blue-600 bg-opacity-10 p-4 text-white mb-4" onClick={()=>{getMoodleLink(address)}}>Get Moodle Login</button></> : <></>
+                }
+              </> : 
+              <><h3 className="text-l text-gray-400 font-extrabold mt-4">No wallet is enabled. Select a Wallet to enabled it</h3></>}
 
-        {/* Connected Wallet information */}
-        <div className="mt-8">
-          {enabledWallet ? (
-            <div className="rounded-md bg-gray-600 bg-opacity-10 p-6">
-              <h3 className="text-xl font-medium tracking-wide">{`Connected to ${enabledWallet.name}`}</h3>
-              <p className="font-semibold uppercase text-txblue text-xs mt-2">
-                Wallet: <span className="text-white">{selectedWallet}</span>
-              </p>
-              <p className="font-semibold uppercase text-txblue text-xs mt-2">
-                Balance:{" "}
-                <span className="text-white">{walletState?.balance}</span>
-              </p>
-              <p className="font-semibold uppercase text-txblue text-xs mt-2">
-                Network:{" "}
-                <span className="text-white">{walletState?.network}</span>
-              </p>
-
-              {walletState?.usedAddresses && (
-                <>
-                  <h4 className="font-semibold uppercase text-txblue text-xs mt-8 mb-4">
-                    used addresses
-                  </h4>
-                  <AddressesTable
-                    addresses={walletState.usedAddresses}
-                  ></AddressesTable>
-                </>
-              )}
-              {walletState?.unusedAddresses && (
-                <>
-                  <h4 className="font-semibold uppercase text-txblue text-xs mt-8 mb-4">
-                    unused addresses
-                  </h4>
-                  <AddressesTable
-                    addresses={walletState.unusedAddresses}
-                  ></AddressesTable>
-                </>
-              )}
-              {walletState?.rewardAddresses && (
-                <>
-                  <h4 className="font-semibold uppercase text-txblue text-xs mt-8 mb-4">
-                    reward addresses
-                  </h4>
-                  <AddressesTable
-                    addresses={walletState.rewardAddresses}
-                  ></AddressesTable>
-                </>
-              )}
-              {walletState?.changeAddress && (
-                <>
-                  <h4 className="font-semibold uppercase text-txblue text-xs mt-8 mb-4">
-                    Change address
-                  </h4>
-                  <AddressesTable
-                    addresses={[walletState.changeAddress]}
-                  ></AddressesTable>
-                </>
-              )}
             </div>
-          ) : null}
+            
+            {/* Displays any error message */}
+            { error? <>
+              <div className="mt-4 rounded-lg border border-red-500 bg-red-600 bg-opacity-10 p-4 text-white">
+                  <h1 className="font-bold">Error</h1>
+                  <h3 className="text-sm text-red-500 mt-2">{`There was an error connecting to the selected wallet: ${error}`}</h3>
+            </div></> : null }
+            
         </div>
-
-        {/* Displays any error message */}
-        {error ? (
-          <p className="text-sm text-red-500 mt-2">{`There was an error connecting to the selected wallet: ${error}`}</p>
-        ) : null}
-      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
--- src/walletsGateway.ts
+++ src/walletsGateway.ts
@@ -3,6 +3,8 @@ import { SUPPORTED_WALLETS } from "./wallets";
 import { IWallet, WalletInfo, WALLET_IDS } from "./wallets/base"
 import { WalletNotAvailableError, WalletNotEnabledError } from "./wallets/errors";
 
+let Buffer = require('buffer/').Buffer;
+
 interface IConnectedWallet {
     getBalance(): Promise<Number>,
     getNetworkId(): Promise<Number>,
@@ -60,7 +62,7 @@ async function getBalance(): Promise<string> {
   if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
   
   const balanceCBORHex = await CONNECTED_WALLET_API.getBalance();
-  const balance = Value.from_hex(String(balanceCBORHex)).coin().to_str();
+  const balance = Value.from_bytes(Buffer.from(balanceCBORHex, "hex")).coin().to_str();
 
   return balance;
 }
@@ -89,30 +91,30 @@ async function getNetwork(): Promise<string>{
 /**
  * Returns a list of all used (included in some on-chain transaction) addresses controlled by the wallet
  */
-async function getUsedAddresses():Promise<string[]> {
+async function getUsedAddresses():Promise<Address[]> {
   if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
   const addresses = await CONNECTED_WALLET_API.getUsedAddresses();
-  return addresses.map(item => Address.from_hex(String(item)).to_bech32(undefined));
+  return addresses;
 }
 
 /**
  * Returns a list of unused addresses controlled by the wallet.
  * @returns 
  */
-async function getUnusedAddresses(): Promise<string[]> {
+async function getUnusedAddresses(): Promise<Address[]> {
   if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
   const addresses = await CONNECTED_WALLET_API.getUnusedAddresses();
-  return addresses.map(item => Address.from_hex(String(item)).to_bech32(undefined));
+  return addresses;
 }
 
 /**
  * Returns the reward addresses owned by the wallet. This can return multiple addresses
  * @returns 
  */
-async function getRewardAddresses(): Promise<string[]> {
+async function getRewardAddresses(): Promise<Address[]> {
   if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
-  const addresses = await CONNECTED_WALLET_API.getRewardAddresses();
-  return addresses.map(item => Address.from_hex(String(item)).to_bech32(undefined));
+  const addresses = await CONNECTED_WALLET_API.getUnusedAddresses();
+  return addresses;
 }
 
 /**
@@ -121,10 +123,10 @@ async function getRewardAddresses(): Promise<string[]> {
  * This can be used as a generic receive address as well.
  * @returns 
  */
-async function getChangeAddress(): Promise<string> {
+async function getChangeAddress(): Promise<Address> {
   if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
   const address = await CONNECTED_WALLET_API.getChangeAddress();
-  return Address.from_hex(String(address)).to_bech32();
+  return address;
 }
 
 /**
@@ -137,6 +139,18 @@ async function getUtxos(): Promise<any[]> {
   return utxos;
 }
 
+/**
+ * Signs the data send in content by using the specified address
+ * Requires the wallet to be enabled
+ * @returns 
+ */
+async function signData(address: Address, content: string) {
+  if (!CONNECTED_WALLET_API) throw new WalletNotEnabledError();
+  const payload = Buffer.from(content, "ascii").toString('hex');
+  const signedData = await CONNECTED_WALLET_API.signData(address, payload);
+  return signedData;
+}
+
 export {
     getAvailableWallets,
     enable,
@@ -146,5 +160,6 @@ export {
     getUnusedAddresses,
     getRewardAddresses,
     getChangeAddress,
+    signData,
     getUtxos,
 }
